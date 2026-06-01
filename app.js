24ca718d81a7f59e77402b769f72a6c9be8c01ff
Ajay Prajapati <prajapatisani9@gmail.com>
2026-06-01 21:38:24 +0530

    mongoDB connection and .env

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+require('dotenv').config();
 const app = express();
 
 app.use(express.json());
@@ -8,7 +8,11 @@ app.get('/', (req, res) => {
   res.send('Server is running');
 });
 
-const PORT = 5000;
+const connectDB = require('./config/db');
+connectDB();
+
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running`);
